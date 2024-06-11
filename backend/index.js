60b9dd1148f2ce05395a1dbe7a20220d2069de81
Hamza <hamzaaziz086@gmail.com>
2024-06-11 20:48:50 +0500

    created user model

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User')
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://root:root@cluster0.mrstoal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body
-    res.json({reqData: {username: username, password: password}})
+
+    try {
+        const newUser = await User.create({username, password})
+        res.json({newUser})
+    } catch (error) {
+        res.status(404).json({message: 'Error creating user'})
+    }
 })
 
 app.listen(4000)
+// mongodb+srv://root:root@cluster0.mrstoal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const {Schema, model} = mongoose
+
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const UserModel = model('User', UserSchema)
+module.exports = UserModel
+
